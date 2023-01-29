e56c36287d22cf03db52a8c387840ccd7751fd63
Henrik <henriksnyamail@gmail.com>
2023-01-29 21:11:07 +0100

    trying some stuff

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ policy_kwargs = dict(n_critics=2, n_quantiles=25,  # activation_fn=th.nn.ReLU,
                      # vf doesnt exist on TQC (?)
                      # pi = actor network, qf = critic network, vf = value network
                      # net_arch=dict(pi=[256, 256], qf=[512, 512, 512])
-                     net_arch=dict(pi=[256, 256], qf=[512, 512, 512])
+                     net_arch=dict(pi=[32, 32], qf=[64, 64, 64])
                      # net_arch=[32, 32]
                      )
 
