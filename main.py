b0183eefa2c416c6fa93ee2809553d93379f17e1
Henrik <henriksnyamail@gmail.com>
2023-01-30 21:40:46 +0100

    asd

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.ddpg.policies import MlpPolicy
 from stable_baselines3.common.noise import OrnsteinUhlenbeckActionNoise
 from stable_baselines3.common.callbacks import BaseCallback
-# from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv
+from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv
 from stable_baselines3.common.vec_env.dummy_vec_env import DummyVecEnv
 from stable_baselines3.common.monitor import Monitor
 # from stable_baselines3.common.vec_env.base_vec_env import VecEnv
@@ -129,7 +129,7 @@ def main():
 
     # wrapped_env = TimeLimit(base_env, max_episode_steps=max_moves_per_episode)
 
-    envs = DummyVecEnv([create_env for _ in range(n_envs)])
+    envs = SubprocVecEnv([create_env for _ in range(n_envs)])
 
     # VecMonitor(env, )
     policy_kwargs = dict(  # n_critics=n_critics, n_quantiles=n_quantiles,  # activation_fn=th.nn.ReLU,
