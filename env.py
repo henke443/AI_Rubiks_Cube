815b8635868f4cf084b8f9577bb3d1a2ddfebc06
Henrik <henriksnyamail@gmail.com>
2023-01-30 15:45:56 +0100

    asd

--- env.py
+++ env.py
@@ -113,20 +113,20 @@ class RubiksEnv(gym.Env):
             moves_after_scramble = max(
                 1, self.cube.total_moves - scramble_moves)
 
-            moves_usage = moves_after_scramble / self._max_moves
-            scramble_usage = scramble_moves / self._n_scramble_moves
+            # moves_usage = moves_after_scramble / self._max_moves
+            # scramble_usage = scramble_moves / self._n_scramble_moves
 
             # toReturn = 1-(percent_moves_usage / scramble_moves)
 
-            alt_score = ((1-moves_usage) + scramble_usage)/2
-            alt_score = max(0, min(1, alt_score**2 + 0.5))
-            score = min(1, (1+self._extra_scramble_moves)/moves_after_scramble)
+            # alt_score = ((1-moves_usage) + scramble_usage)/2
+            # alt_score = max(0, min(1, alt_score**2 + 0.5))
+            score = min(1, (scramble_moves)/moves_after_scramble)
 
             # score = (moves_usage - 1) ** 2 * (scramble_usage - 1/self._n_scramble_moves) ** 2 \
             #    / ((moves_usage - 1) ** 2 + (scramble_usage - 1/self._n_scramble_moves) ** 2)
 
             print("Solved once! Score:", score,
-                  "Scramble moves:", 1+self._extra_scramble_moves, "solved in:", moves_after_scramble, "alt_score:", alt_score)
+                  "Scramble moves:", 1+self._extra_scramble_moves, "solved in:", moves_after_scramble)  # "alt_score:", alt_score)
             self._solved_before = 1
         if score < -1 or score > 1:
             print("WTF:", score, distance)
