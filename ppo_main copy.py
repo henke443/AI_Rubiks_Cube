0dad115ff30f1e5b47a0a9c027e106bbe6471935
Henrik <henriksnyamail@gmail.com>
2023-01-30 21:19:52 +0100

    teasd

---
+++ ppo_main copy.py
@@ -0,0 +1,200 @@
+from __future__ import annotations
+
+import env
+from c2 import Cube
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.ddpg.policies import MlpPolicy
+from stable_baselines3.common.noise import OrnsteinUhlenbeckActionNoise
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv
+from stable_baselines3.common.vec_env.dummy_vec_env import DummyVecEnv
+from stable_baselines3.common.monitor import Monitor
+# from stable_baselines3.common.vec_env.base_vec_env import VecEnv
+# from stable_baselines3.common.vec_env.vec_monitor import VecMonitor
+# from stable_baselines3 import DDPG
+from gym.wrappers.time_limit import TimeLimit
+# from sb3_contrib import TQC
+# from sb3_contrib import QRDQN
+from stable_baselines3 import PPO
+
+
+import numpy as np
+import time
+import torch as th
+
+
+class CustomCallback(BaseCallback):
+    """
+    A custom callback that derives from ``BaseCallback``.
+
+    :param verbose: (int) Verbosity level 0: not output 1: info 2: debug
+    """
+
+    def __init__(self, callback, verbose=0):
+        super(CustomCallback, self).__init__(verbose)
+        self.callback = callback
+        # Those variables will be accessible in the callback
+        # (they are defined in the base class)
+        # The RL model
+        # self.model = None  # type: BaseRLModel
+        # An alias for self.model.get_env(), the environment used for training
+        # self.training_env = None  # type: Union[gym.Env, VecEnv, None]
+        # Number of time the callback was called
+        # self.n_calls = 0  # type: int
+        # self.num_timesteps = 0  # type: int
+        # local and global variables
+        # self.locals = None  # type: Dict[str, Any]
+        # self.globals = None  # type: Dict[str, Any]
+        # The logger object, used to report things in the terminal
+        # self.logger = None  # type: logger.Logger
+        # # Sometimes, for event callback, it is useful
+        # # to have access to the parent object
+        # self.parent = None  # type: Optional[BaseCallback]
+
+    def _on_rollout_start(self) -> None:
+        """
+        A rollout is the collection of environment interaction
+        using the current policy.
+        This event is triggered before collecting new samples.
+        """
+
+        pass
+
+    def _on_step(self) -> bool:
+        """
+        This method will be called by the model after each call to `env.step()`.
+
+        For child callback (of an `EventCallback`), this will be called
+        when the event is triggered.
+
+        :return: (bool) If the callback returns False, training is aborted early.
+        """
+        # print("_on_step:", self.locals, self.globals)
+        self.callback({
+            "steps": self.num_timesteps,
+            "calls": self.n_calls,
+            "total_steps": self.locals["total_timesteps"],
+            "learning_starts": self.locals["learning_starts"] if "learning_starts" in self.locals else None,
+            "rewards": self.locals["rewards"] if "rewards" in self.locals else None,
+            "dones": self.locals["dones"] if "dones" in self.locals else None,
+            "infos": self.locals["infos"] if "infos" in self.locals else None,
+        })
+
+        return True
+
+    def _on_rollout_end(self) -> None:
+        """
+        This event is triggered before updating the policy.
+        """
+        pass
+
+    def _on_training_end(self) -> None:
+        """
+        This event is triggered before exiting the `learn()` method.
+        """
+        pass
+
+
+def main():
+    n_envs = 10
+    log_interval = 200
+    total_timesteps = np.int64(1e6)
+    learning_starts = 100
+
+    batch_size = 256  # 2**14
+    max_moves_per_episode = 100
+    n_scramble_moves = 50
+    learning_rate = 3e-5
+
+    pi = [256, 256]
+    # qf = [512, 512, 512]
+
+    top_quantiles_to_drop_per_net = 2
+    n_critics = 2
+    n_quantiles = 50
+    gamma = 0.99
+    tau = 0.005
+
+    def create_env(*params):
+        print("Create env params:", params)
+        new_env = env.RubiksEnv(
+            moves_per_step=1, n_scramble_moves=n_scramble_moves, max_moves=max_moves_per_episode)
+        return Monitor(
+            TimeLimit(new_env, max_episode_steps=max_moves_per_episode)
+        )
+
+    base_env = env.RubiksEnv(
+        moves_per_step=1, n_scramble_moves=n_scramble_moves, max_moves=max_moves_per_episode)
+    check_env(base_env)
+
+    # wrapped_env = TimeLimit(base_env, max_episode_steps=max_moves_per_episode)
+
+    envs = SubprocVecEnv([create_env for _ in range(n_envs)])
+
+    # VecMonitor(env, )
+    policy_kwargs = dict(  # n_critics=n_critics, n_quantiles=n_quantiles,  # activation_fn=th.nn.ReLU,
+        # vf doesnt exist on TQC (?)
+        # pi = actor network, qf = critic network, vf = value network
+        # net_arch=dict(pi=[256, 256], qf=[512, 512, 512])
+        net_arch=dict(pi=pi)
+        # net_arch=[32, 32]
+    )
+
+    # action_noise = OrnsteinUhlenbeckActionNoise(
+    #    mean=np.zeros(wrapped_env.action_space.shape[-1]), sigma=float(0.2) * np.ones(wrapped_env.action_space.shape[-1]))
+
+    # policy_kwargs = dict(n_critics=2, n_quantiles=25, n_env=)
+    model = PPO("MlpPolicy", envs,
+                # top_quantiles_to_drop_per_net=top_quantiles_to_drop_per_net,
+                # ent_coef="auto",
+                # verbose=1,
+                log_interval=100,
+                n_steps=total_timesteps,
+                # n_epochs=20,
+                learning_rate=learning_rate,
+                batch_size=batch_size,
+                # optimize_memory_usage=False,
+                # action_noise=action_noise,
+                policy_kwargs=policy_kwargs,
+                gae_lambda=0.9,
+                clip_range=0.2
+                # learning_rate=learning_rate,
+                # learning_starts=learning_starts,
+                # gamma=gamma,
+                # tau=tau
+                )
+    """
+    n_epochs: int = 10, gamma: float = 0.99, gae_lambda: float = 0.95, clip_range: float | Schedule = 0.2, clip_range_vf: float | Schedule | None = None, normalize_advantage: bool = True, ent_coef: float = 0, vf_coef: float = 0.5, max_grad_norm: float = 0.5, use_sde: bool = False, sde_sample_freq: int = -1, target_kl: float | None = None
+    """
+
+    def callback(options):
+        # print("options:", options)
+        # wrapped_env.reset(options=options)
+        # model.env.set_attr("cur_steps", options["steps"])
+        # model.env.set_attr("total_steps", options["total_steps"])
+        # model.env.set_attr("steps", options["steps"])
+        # model.env.set_attr("total_steps", options["total_steps"])
+        model.env.env_method("set_steps", options["steps"])
+        model.env.env_method("set_total_steps", options["total_steps"])
+        # print("in step:", model.env.get_attr("episode_returns"))
+        # print(model.get_env().get_attr("base_moves"))
+        # model.env.set_attr("base_moves", ["poop"])
+        # print(model.env.get_attr("cur_steps"))
+        # print(model.env.get_attr("total_steps"))
+        # print()
+        # if "episode" in options["infos"][0]:
+        # model.env.env_method("reset", options=options)
+        # model.env.set_attr("total_steps", options["total_steps"])
+        # print("callback options:", options)
+        # elif len(options["infos"][0]):
+        #    print("wtf?", options)
+
+    model.learn(total_timesteps=total_timesteps, log_interval=log_interval,
+                progress_bar=True,
+                callback=CustomCallback(callback, verbose=0)
+                )
+    model.save("tqc_rubiks")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tqc_env copy.py
@@ -0,0 +1,271 @@
+from __future__ import annotations
+
+from c2 import Cube
+import sys
+import gym
+from gym import spaces
+import random
+from time import perf_counter
+import numpy as np
+
+
+# sys.modules["gym"] = gym
+
+
+# Good docs on this: https://www.gymlibrary.dev/content/environment_creation/
+class RubiksEnv(gym.Env):
+    """Custom Environment that follows gym interface"""
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, moves_per_step=1, terminate_after_n_moves: int | str = False, n_scramble_moves=40, max_moves=0):
+        super(RubiksEnv, self).__init__()
+
+        self.steps = 0
+        self.total_steps = 0
+
+        self._n_scramble_moves = n_scramble_moves
+        self._terminate_after_n_moves = terminate_after_n_moves
+        self.all_moves = ["U", "U'", "L", "L'", "B",
+                          "B'", "R", "R'", "F", "F'", "D", "D'"]
+
+        self.base_moves = ["U", "L", "B", "R", "F", "D"]
+
+        self.moves_per_step = moves_per_step
+
+        actions_min = [np.float32(-1)]*len(self.base_moves)*moves_per_step
+        actions_max = [np.float32(1)]*len(self.base_moves)*moves_per_step
+        self.action_space = spaces.Box(
+            np.array(actions_min), np.array(actions_max)
+        )
+
+        self.observation_space = spaces.Box(
+            0, 5, shape=(54,), dtype=np.int8
+        )
+
+        self._max_moves = max_moves
+        self._extra_scramble_moves = 0
+
+        self._has_reset_logged = False
+        self._solved_before = 0
+
+    def _vector_action_to_action(self, vec):
+        # val = next(([i, x] for i, x in enumerate(vec) if x), None)
+        maxv = np.max(vec)
+        minv = np.min(vec)
+
+        move = self.base_moves[
+            np.where(vec == maxv)[0][0]
+        ] if abs(maxv) >= abs(minv) else self.base_moves[
+            np.where(vec == minv)[0][0]
+        ] + "'"
+
+        # print("move:", move, "\n")
+        # action_moves = []
+        # for v in ([i, x] for i, x in enumerate(vec) if x):
+        #    _move = self.base_moves[v[0] % self.moves_per_step]
+        #    if v[1] < 0:
+        #        _move += "'"
+        #        action_moves.append(_move)
+        #    if v[1] > 0:
+        #        action_moves.append(_move)
+        return move  # " ".join(action_moves)
+
+    def _get_obs(self):
+        color_map = {
+            "Y": 0,
+            "O": 1,
+            "G": 2,
+            "R": 3,
+            "B": 4,
+            "W": 5
+        }
+        return np.array([color_map[self.cube.get_color(x)] for x in self.cube._data], dtype=np.int8)
+
+    def _get_info(self):
+        solved = self._solved_obs
+        # print("Solved:", solved)
+        current = self._get_obs()
+        # print("Current:", current)
+
+        distance = 1-(
+            sum([
+                1 if v == current[i] else 0
+                for i, v in enumerate(solved)
+            ])
+        ) / len(solved)
+
+        # distance = min(1, max(0, distance))
+
+        # speed = 1/self.cube.total_moves
+
+        # score = speed * 0.1 + (1-distance) * 0.9
+        # score = 1-distance  # score of 1 when distance is 0 = solved
+
+        if hasattr(self, "_scramble_distance"):
+            pass
+            # Remove scramble_distance from score
+            # if scramble is solved (scramble_distance = 0)
+            # and score is 1, then remove 1 and score is 0
+            # score = score - (1-self._scramble_distance)
+            # Clip it between -1 and 1 (should already be the case but I tried before with 0 <= x <= 1)
+            # score = max(0, min(1, score))
+
+        score = -1
+        if distance == 0:
+            scramble_moves = 1+self._extra_scramble_moves
+            moves_after_scramble = max(
+                1, self.cube.total_moves - scramble_moves)
+
+            # moves_usage = moves_after_scramble / self._max_moves
+            # scramble_usage = scramble_moves / self._n_scramble_moves
+
+            # toReturn = 1-(percent_moves_usage / scramble_moves)
+
+            score = 1  # moves_after_scramble/self._max_moves
+
+            # alt_score = ((1-moves_usage) + scramble_usage)/2
+            # alt_score = max(0, min(1, alt_score**2 + 0.5))
+            # score = 0.5*min(1, scramble_moves/self._n_scramble_moves) + \
+            #    0.5*min(1, scramble_moves/moves_after_scramble)
+
+            # score *= self._max_moves
+
+            # score = (moves_usage - 1) ** 2 * (scramble_usage - 1/self._n_scramble_moves) ** 2 \
+            #    / ((moves_usage - 1) ** 2 + (scramble_usage - 1/self._n_scramble_moves) ** 2)
+
+            # print("\nSolved once! Score:", score,
+            #      "Scramble moves:", 1+self._extra_scramble_moves, "solved in:", moves_after_scramble)  # "alt_score:", alt_score)
+            self._solved_before = 1
+        # if score < -1 or score > 1:
+        #    print("WTF:", score, distance)
+        #    exit()
+        return {
+            "distance": distance,
+            # "speed": speed,
+            "score": score
+        }
+
+    def set_steps(self, step):
+        self.steps = step
+
+    def set_total_steps(self, total_steps):
+        self.total_steps = total_steps
+
+    def step(self, action):
+
+        if hasattr(self, "episode_returns"):
+            print("in step:", self.episode_returns)
+        # print("asd:", self.steps)
+        move = self._vector_action_to_action(action)
+
+        self.cube.moves(move)
+
+        observation = self._get_obs()
+        info = self._get_info()
+
+        # An episode is done if cube is solved
+        terminated = info["distance"] == 0
+
+        score = info["score"]
+
+        # Also terminate if moves are greater than some amount this episode... This is bad because in new versions of gym
+        # it works differently, where you return terminated, truncated, info instead of terminated, info.
+        # I tried using TimeLimit which should theoretically be a better approach but then my model suicides and
+        # makes a policy that is as shitty as possible, not sure why.
+        # if self._terminate_after_n_moves != False and self.cube.total_moves >= self._terminate_after_n_moves+self._n_scramble_moves:
+        #    terminated = True
+
+        # if self.render_mode == "human":
+        #    self._render_frame()
+
+        return observation, score, terminated, {}  # truncated, {}
+
+    def reset(self, options=None):
+        # We need the following line to seed self.np_random
+        # super().reset(seed=seed)
+        # random.seed(seed)
+
+        if options:
+            print("reset options:", options)
+
+        self._solved_before = 0
+
+        # print("reset options:", options)
+        self._extra_scramble_moves = 0
+
+        self._extra_scramble_moves = round(
+            np.tanh((self.steps / (self.total_steps+1))
+                    ) * self._n_scramble_moves
+        )
+
+        if self._extra_scramble_moves > 0:
+            if bool(random.getrandbits(1)):
+                self._extra_scramble_moves = np.random.randint(
+                    0, self._extra_scramble_moves)
+
+        # if not self._has_reset_logged and self.steps % 500 < 100:
+        #    self._has_reset_logged = True
+        #    print("reset() steps extra_scramble_moves:",
+        #          self._extra_scramble_moves)
+            # print("asd", self.total_steps, self.steps)
+        # else:
+        #    self._has_reset_logged = False
+
+        self.cube = Cube()
+
+        self._solved_obs = self._get_obs()
+
+        scramble_moves = []
+        while len(scramble_moves) != 1 + self._extra_scramble_moves:
+            for i in range(0, 1 + self._extra_scramble_moves):
+                scramble_moves.append(random.choice(self.base_moves))
+
+                while i > 0 and scramble_moves[i-1] == scramble_moves[i]:
+                    scramble_moves[i] = random.choice(self.base_moves)
+
+        scramble_moves = " ".join([
+            x + ("'" if bool(random.getrandbits(1)) else "")
+            for x in scramble_moves
+        ])
+
+        if len(scramble_moves) < 1:
+            print("Scramble moves 2 was less than 1")
+            print("scramble_moves", scramble_moves)
+            exit()
+        # print("Scramble moves:", scramble_moves, self._extra_scramble_moves)
+
+        # print("asd", self._extra_scramble_moves)
+
+        self.cube.moves(scramble_moves)
+
+        # set the scramble "distance to solved", 0 is solved, 1 is furthest away from solved
+        self._scramble_distance = self._get_info()["distance"]
+
+        observation = self._get_obs()
+        # info = self._get_info()
+
+        # if self.render_mode == "human":
+        #    self.render()
+
+        # print("obs:", observation, "len:", len(observation))
+        return observation
+
+    def render(self, mode='human'):
+        print(self.cube.print())
+        pass
+
+    def close(self):
+        pass
+
+
+if __name__ == "__main__":
+    env = RubiksEnv()
+    env.reset()
+    print(env._get_info())
+    print(env._get_obs())
+    # env.render()
+    print("Then doing R U R:")
+    env.cube.moves("R U R")
+    print(env._get_info())
+    print(env._get_obs())
+    # env.render()
