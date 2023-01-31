bb60459f4b5469023f8ac99eae241bf97a6b4b50
Henrik <henriksnyamail@gmail.com>
2023-01-31 23:47:26 +0100

    asd

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ def main():
         # vf doesnt exist on TQC (?)
         # pi = actor network, qf = critic network, vf = value network
         # net_arch=dict(pi=[256, 256], qf=[512, 512, 512])
-        net_arch=dict(pi=pi, qf=qf),
+        net_arch=[128, 128]
         n_quantiles=25,
         features_extractor_class=CustomCNN,
         features_extractor_kwargs=dict(features_dim=128),
