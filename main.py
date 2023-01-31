b7cc26153d5e401333bf3f9b9e96d535dfdfbe0b
Henrik <henriksnyamail@gmail.com>
2023-01-31 18:13:32 +0100

    asd

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ def main():
         # pi = actor network, qf = critic network, vf = value network
         # net_arch=dict(pi=[256, 256], qf=[512, 512, 512])
         net_arch=pi,
-        n_quantiles=25
+        n_quantiles=25,
         normalize_images=False,
         # net_arch=[32, 32]
     )
