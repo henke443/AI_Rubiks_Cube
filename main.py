725fd87973c9870410efc1cb84f96140e251760d
Henrik <henriksnyamail@gmail.com>
2023-01-31 19:58:20 +0100

    asd

--- main.py
+++ main.py
@@ -140,7 +140,7 @@ def main():
     learning_starts = 100
 
     batch_size = 64  # 2**14
-    max_moves_per_episode = 20
+    max_moves_per_episode = 100
     n_scramble_moves = 10
     learning_rate = 3e-5
 
