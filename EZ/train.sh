d3eb47f28424f18f28b98aa2166aad341bdc7970
Henrik <henriksnyamail@gmail.com>
2023-02-03 13:45:25 +0100

    asd

--- EZ/train.sh
+++ EZ/train.sh
@@ -4,6 +4,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 python main.py --env BreakoutNoFrameskip-v4 --case rubik --opr train --force \
   --num_gpus 1 --num_cpus 1 --cpu_actor 1 --gpu_actor 1 \
+  --object_store_memory 1610612736
   --seed 0 \
   --p_mcts_num 4 \
   --use_priority \
