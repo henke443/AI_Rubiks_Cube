2e8b19c872c658c3c507065535be441cf336f8b6
Henrik <henriksnyamail@gmail.com>
2023-02-03 15:01:31 +0100

    asd

--- EZ/config/rubik/__init__.py
+++ EZ/config/rubik/__init__.py
@@ -49,7 +49,7 @@ class RubikConfig(BaseConfig):
             transition_num=1,
             # frame skip & stack observation
             frame_skip=1,
-            stacked_observations=4,
+            stacked_observations=1,
             # coefficient
             reward_loss_coeff=1,
             value_loss_coeff=0.25,
