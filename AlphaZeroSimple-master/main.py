a184657cf2ce0fcf09f28f08bf7164748122bd59
Henrik <henriksnyamail@gmail.com>
2023-02-06 11:11:43 +0100

    asd

--- AlphaZeroSimple-master/main.py
+++ AlphaZeroSimple-master/main.py
@@ -16,7 +16,7 @@ args = {
 
     # Number of full games (episodes) to run during each iteration
     # (outer loop of MCTS, aka number of batches of num_simulations MCTS simulations)
-    'numEps': 50,
+    'numEps': 64,
 
     # Total number of MCTS simulations to run when deciding on a move to play
     'num_simulations': 200,
