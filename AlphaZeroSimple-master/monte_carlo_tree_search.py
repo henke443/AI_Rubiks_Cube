fad4e1eb3747cd94bb4e9fca1c3c1d14d21bfd79
Henrik <henriksnyamail@gmail.com>
2023-02-06 16:10:12 +0100

    asd

--- AlphaZeroSimple-master/monte_carlo_tree_search.py
+++ AlphaZeroSimple-master/monte_carlo_tree_search.py
@@ -126,7 +126,7 @@ class MCTS:
             state = parent.state
             # Now we're at a leaf node and we would like to expand
             # Players always play from their own perspective
-            print("len(search_path)", len(search_path))
+            # print("len(search_path)", len(search_path))
             next_state = self.game.get_next_state(
                 state, action=action, total_moves=len(search_path))
             # Get the board from the perspective of the other player
