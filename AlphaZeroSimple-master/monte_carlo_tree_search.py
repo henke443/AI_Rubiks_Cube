aa31bcd30ea18fb4028e7f78b47921687843b0b7
Henrik <henriksnyamail@gmail.com>
2023-02-06 10:08:50 +0100

    asd

--- AlphaZeroSimple-master/monte_carlo_tree_search.py
+++ AlphaZeroSimple-master/monte_carlo_tree_search.py
@@ -149,7 +149,7 @@ class MCTS:
                 print("Solved once, parent state, action:", parent.state, action)
                 print("Parent would choose:",
                       parent.select_action(temperature=0))
-                print("resulting state:", state)
+                print("resulting state:", next_state)
 
         # print("mcts run ended")
         return root
