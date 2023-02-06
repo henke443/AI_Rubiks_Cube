04281b87401506af97d433b1a071d23244ef7e40
Henrik <henriksnyamail@gmail.com>
2023-02-06 02:14:57 +0100

    asd

--- AlphaZeroSimple-master/monte_carlo_tree_search.py
+++ AlphaZeroSimple-master/monte_carlo_tree_search.py
@@ -131,10 +131,7 @@ class MCTS:
             # The value of the new state from the perspective of the other player
             value = self.game.get_reward(next_state)
 
-            if depth >= self.max_depth and value is None:
-                value = -1
-
-            if value is None:
+            if value is None and depth < self.max_depth:
                 # If the game has not ended:
                 # EXPAND
                 action_probs, value = model.predict(next_state)
