dc9c15f35c3c16e3dcee6b8577c46bc0040787e5
Henrik <henriksnyamail@gmail.com>
2023-02-07 17:40:15 +0100

    asd

--- AlphaZeroSimple-master/graph.py
+++ AlphaZeroSimple-master/graph.py
@@ -254,7 +254,8 @@ class RubiksExample:
 
         # print("examples actions:", [x[1] for x in examples])
         # print("num examples:", len(examples))
-        # print("lens:", lens)
+        print("lens:", lens)
+        print("vals:", [x[2] for x in examples])
         print("avg length:", sum(lens)/len(lens))
 
         return examples
