b0f6517e1edc4ef0a1a6150e560bbe0beb368d3e
Henrik <henriksnyamail@gmail.com>
2023-02-07 17:53:18 +0100

    asd

--- AlphaZeroSimple-master/graph.py
+++ AlphaZeroSimple-master/graph.py
@@ -255,11 +255,11 @@ class RubiksExample:
 
             # print("examples actions:", [x[1] for x in examples])
             # print("num examples:", len(examples))
-            # if iter % 50 == 0:
-
-                # print("lens:", lens)
-                # print("vals:", [x[2] for x in examples])
-                # print("avg length:", sum(lens)/len(lens))
+            if iter % 50 == 0:
+                print("episode doneness:", str((i/self.n_iters)*100)+"%")
+                print("lens:", lens)
+                print("vals:", [x[2] for x in examples])
+                print("avg length:", sum(lens)/len(lens))
 
         return examples
 
