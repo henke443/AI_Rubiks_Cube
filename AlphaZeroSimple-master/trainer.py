90e4f8e56c07008226933e16727e9d5e72e4df27
Henrik <henriksnyamail@gmail.com>
2023-02-07 03:34:20 +0100

    asd

--- AlphaZeroSimple-master/trainer.py
+++ AlphaZeroSimple-master/trainer.py
@@ -21,7 +21,7 @@ class Trainer:
         self.step = 0
 
     def execute_episode(self):
-        return graph.generate(self.model, 25, 100)
+        return graph.generate(self.model, 25, 20)
 
     def learn(self):
         for i in range(1, self.args['numIters'] + 1):
