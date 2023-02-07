0f46c8de9802d92a70cb6a50227ff2812e7e1331
Henrik <henriksnyamail@gmail.com>
2023-02-07 02:55:27 +0100

    asd

--- AlphaZeroSimple-master/main.py
+++ AlphaZeroSimple-master/main.py
@@ -4,36 +4,42 @@ from game import RubiksGame
 from model import RubiksModel
 from trainer import Trainer
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-args = {
-    # batch_size of mcts simulations when training neural nets
-    'batch_size': 64,
+def main():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # Total number of training iterations
-    # (outer loop of episodes, aka number of batches of numEps episodes)
-    'numIters': 500,
+    args = {
+        # batch_size of mcts simulations when training neural nets
+        'batch_size': 64,
 
-    # Number of full games (episodes) to run during each iteration
-    # (outer loop of MCTS, aka number of batches of num_simulations MCTS simulations)
-    'numEps': 10,
+        # Total number of training iterations
+        # (outer loop of episodes, aka number of batches of numEps episodes)
+        'numIters': 500,
 
-    # Total number of MCTS simulations to run when deciding on a move to play
-    'num_simulations': 100,
+        # Number of full games (episodes) to run during each iteration
+        # (outer loop of MCTS, aka number of batches of num_simulations MCTS simulations)
+        'numEps': 10,
 
-    'numItersForTrainExamplesHistory': 20,
-    # Number of epochs of training per iteration
-    'epochs': 2,
-    # location to save latest set of weights
-    'checkpoint_path': 'latest.pth'
-}
+        # Total number of MCTS simulations to run when deciding on a move to play
+        'num_simulations': 100,
 
-game = RubiksGame()
-game.env.reset(fixed_extra_scrambles=0)
-board_size = game.get_board_size()
-action_size = game.get_action_size()
+        'numItersForTrainExamplesHistory': 20,
+        # Number of epochs of training per iteration
+        'epochs': 2,
+        # location to save latest set of weights
+        'checkpoint_path': 'latest.pth'
+    }
 
-model = RubiksModel(board_size, action_size, device)
+    game = RubiksGame()
+    game.env.reset(fixed_extra_scrambles=0)
+    board_size = game.get_board_size()
+    action_size = game.get_action_size()
 
-trainer = Trainer(game, model, args)
-trainer.learn()
+    model = RubiksModel(board_size, action_size, device)
+
+    trainer = Trainer(game, model, args)
+    trainer.learn()
+
+
+if __name__ == "__main__":
+    main()
